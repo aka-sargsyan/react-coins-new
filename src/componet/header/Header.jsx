eda98b9de7baa9d1f50c00803ef45a99e812521d
aka-sargsyan <akasargsyan87@gmail.com>
2020-04-14 00:04:33 +0400

    added new div

--- src/componet/header/Header.jsx
+++ src/componet/header/Header.jsx
@@ -10,6 +10,7 @@ const Header = (props) => {
     <Container className={`pb80 ${moduleCss.header}`}>
       <Row>
         <Col>
+        <div>hello world</div>
           <header className={`flexStart flexAlinCenter flexBetween ${moduleCss.headerContainer}`}>
             <NavLink to={`/`}>
               <img src={props.logo} alt="" width='240' height='120' />
